78703969f21aa108fe1877e7535ea76fb04df00c
Roberto Chavarria <robertomorais107@gmail.com>
2025-07-12 18:24:13 -0300

    Add more tests for items_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,13 +1,51 @@
 require_relative '../lib/item'
 
 RSpec.describe Item do
-	describe 'initialize' do
+  let(:item) { Item.new("book", 12.49, 2) }
 
-		let(:item) { Item.new("book", 12.49, 2) }
-		it 'creates an item with name, price, and quantity' do
-			expect(item.name).to eq("book")
-			expect(item.price).to eq(12.49)
-			expect(item.quantity).to eq(2)
-		end
-	end
+  describe 'initialize' do
+    it 'creates an item with name, price, and quantity' do
+      expect(item.name).to eq("book")
+      expect(item.price).to eq(12.49)
+      expect(item.quantity).to eq(2)
+    end
+  end
+
+  describe 'imported?' do
+    context 'when item is imported' do
+      let(:item) { Item.new("imported book", 12.49, 2) }
+
+      it { expect(item.imported?).to be true }
+    end
+
+    context 'when item is not imported' do
+      let(:item) { Item.new("book", 12.49, 2) }
+
+      it { expect(item.imported?).to be false }
+    end
+  end
+
+  describe 'tax_exempt?' do
+    context 'when an item is exempt' do
+      expected_exempted_items = ['book', 'chocolate', 'headache pills', 'pills']
+
+      expected_exempted_items.each do |name|
+        it "marks '#{name}' as tax-exempt for '#{name}'" do
+          item = Item.new(name, 12.49, 2)
+          expect(item.tax_exempt?).to be true
+        end
+      end
+    end
+
+    context 'when an item is not exempt' do
+      non_exempted_items = ['perfume', 'music CD']
+
+      non_exempted_items.each do |name|
+        it "does not mark '#{name}' as tax-exempt" do
+          item = Item.new(name, 10.0, 1)
+          expect(item.tax_exempt?).to be false
+        end
+      end
+    end
+  end
 end
